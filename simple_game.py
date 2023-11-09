d001178acf9ff3be1aeadfe9814cde9aae0a57f2
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-08 16:25:44 -0800

    Complete game system

--- simple_game.py
+++ simple_game.py
@@ -1,14 +1,25 @@
+"""
+Chun-Wei, Mo
+A01375071
+"""
 import random
 
 
 def make_room():
-    room = {0: 'Entrance', 1: 'Poison gas',
-            2: 'Spike trap', 3: 'Spirit fountain', 4: 'Camp fire', 5: 'Empty room'}
-    room_seed = random.randint(1, 4)
+    """
+    Return a randon room from 5 kinds fo room.
+
+    :postcondtion: return a randon room from 5 kinds of room
+    :return: a string
+    """
+    room = {0: 'Empty room',
+            1: 'Spike trap', 2: 'Poison gas', 3: 'Camp fire', 4: 'Spirit fountain'}
+    room_seed = random.randint(0, 4)
     return room[room_seed]
 
 
-def room_effect(board, location: tuple, character):
+def room_effect(board, character):
+    location = (character['X-coordinate'], character['Y-coordinate'])
     if board[location] == 'Poison gas':
         print("You enter a poison chamber. Poison gas has caused 1 damage.")
         character['Current HP'] -= 1
@@ -30,7 +41,7 @@ def make_board(rows, columns):
             position = (row, column)
             room = make_room()
             if row == 0 and column == 0:
-                room = 0
+                room = "Entrance"
             board[position] = room
     return board
 
@@ -43,25 +54,19 @@ def board_size(board):
             south_wall = grid[0]
         if grid[1] > east_wall:
             east_wall = grid[1]
-    return (south_wall, east_wall)
+    return south_wall, east_wall
 
 
 def make_character():
     return {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 5}
 
 
-def describe_current_location(board, character):
-    # south_wall, east_wall = board_size(board)
+def describe_current_location(character):
     location = (character['X-coordinate'], character['Y-coordinate'])
-    print(
-        f"You are at X: {character['X-coordinate']}, Y: {character['Y-coordinate']}.")
     if location == (0, 0):
         print("You stand at the entrance, start your advance to SE corner.")
-    print()
-    # if character['X-coordinate'] > east_wall or character['X-coordinate'] < 0 or character['Y-coordinate'] > south_wall or character['Y-coordinate'] < 0:
-    #     print("!!WARNING!!: Out of the board")
-    #     print()
-    return location
+        print()
+    print(f"You are at X: {character['X-coordinate']}, Y: {character['Y-coordinate']}.")
 
 
 def describe_current_hp(character):
@@ -71,10 +76,10 @@ def describe_current_hp(character):
 
 def get_user_choice():
     direction_list = (0, 1, 2, 3)
-    direction = ''
     while True:
         print('The direction you can choose 0: North, 1: East, 2: South, 3: West')
         direction = int(input("Enter a number to choose direction: "))
+        print()
         if direction in direction_list:
             break
         print("Incorrect input, try again.")
@@ -102,7 +107,6 @@ def validate_move(board, character, direction):
     return can_move
 
 
-# move_character(character)
 def move_character(character, direction):
     if direction == 0:
         character["Y-coordinate"] -= 1
@@ -116,14 +120,6 @@ def move_character(character, direction):
     if direction == 3:
         character["X-coordinate"] -= 1
         print("moving toward West...")
-
-
-def check_if_goal_attained(board, character, achieved_goal):
-    south_wall, east_wall = board_size(board)
-    if character["X-coordinate"] == east_wall and character["Y-coordinate"] == south_wall:
-        achieved_goal = True
-        print("Congratulation! You achieve the goal!")
-    return achieved_goal
 
 
 def check_for_foes():
@@ -141,68 +137,60 @@ def guessing_game(character):
     if attack == foe_randint:
         print("Nice attack! The foe is killed!")
     else:
-        print("Miss. The foe attack you. You lose 1 HP.")
+        print("Miss. The foe makes a counterattack. You lose 1 HP.")
         character["Current HP"] -= 1
 
 
 def is_alive(character):
     if character["Current HP"] <= 0:
+        print("You dead...")
+        print("Close the game.")
         return False
     else:
         return True
 
 
+def check_if_goal_attained(board, character):
+    south_wall, east_wall = board_size(board)
+    if character["X-coordinate"] == east_wall and character["Y-coordinate"] == south_wall:
+        achieved_goal = True
+        print("Congratulation! You achieve the goal!")
+        return achieved_goal
+
+
 def game():
-    rows = 3
-    columns = 3
+    rows = 4
+    columns = 4
     board = make_board(rows, columns)
     print(board)
     south_wall, east_wall = board_size(board)
     print(f"This is a {south_wall} X {east_wall} board.")
     character = make_character()
-    describe_current_location(board, character)
     achieved_goal = False
+
     while not achieved_goal:
-        # // Tell the user where they are
+        describe_current_location(character)
         direction = get_user_choice()
         valid_move = validate_move(board, character, direction)
         if valid_move:
             move_character(character, direction)
-            location = describe_current_location(board, character)
-            room_effect(board, location, character)
+            room_effect(board, character)
+            if not is_alive(character):
+                break
             describe_current_hp(character)
-    # there_is_a_challenger = check_for_foes()
-    # if there_is_a_challenger:
-    # guessing_game(character)
-        achieved_goal = check_if_goal_attained(board, character, achieved_goal)
-    # else:
-    # // Tell the user they can’t go in that direction
-    # // Print end of game stuff like congratulations or sorry you died
+
+        there_is_a_challenger = check_for_foes()
+        if there_is_a_challenger:
+            guessing_game(character)
+            describe_current_hp(character)
+            if not is_alive(character):
+                break
+
+        achieved_goal = check_if_goal_attained(board, character)
 
 
 def main():
     game()
-    # board = make_board(4, 4)
-    # print(board)
-
-    # character = make_character()
-    # print(character)
-
-    # get_user_choice()
-    # print(1 in [0, 1, 2, 3])
-    # direction = 3
-    # character = {'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 5}
-    # print(f"direction: {direction}")
-    # print(validate_move(board, player, direction))
-
-    # location = describe_current_location(board, character)
-    # print(location)
-    # room_effect(board, location, character)
-    # print(character)
-
-    # achieved_goal = False
-    # achieved_goal = check_if_goal_attained(board, character, achieved_goal)
-    # print(f'achieve goal? {achieved_goal}')
 
 
 if __name__ == "__main__":
