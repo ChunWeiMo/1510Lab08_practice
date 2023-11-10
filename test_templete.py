ea703d391ec2bf9d5fa23a7d19f9046c85cd79cf
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-10 01:26:13 -0800

    Modify templete

--- test_templete.py
+++ test_templete.py
@@ -1,3 +1,4 @@
+import io
 from unittest import TestCase
 from unittest.mock import patch
 from unittest import main
@@ -6,6 +7,8 @@ from simple_game import
 
 class Test(TestCase):
     @patch('random.randint', return_value=0)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=[5])
     def test_(self, _):
         pass
     
